aa2122ddacf6823efa843d163975a9788ea259fd
Koorosh Fotouhi <kooroshFA@outlook.com>
2022-05-02 11:47:29 +0430

    Add Core Mechanics/Movement

---
+++ Runner2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpSpeed;
+    private bool isOnGround;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * moveSpeed);
+
+        if(Input.GetKeyDown(KeyCode.Space) )
+        {
+            rb.AddForce(Vector2.up * jumpSpeed);
+            isOnGround = false;
+            Debug.Log("Space Pressed" + rb.velocity);
+        }
+
+    }
+}
